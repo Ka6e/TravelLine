201d55dfec544002486fe8df270d369e81b8a849
Ka6e <ivan81747@gmail.com>
2025-07-13 21:19:17 +0300

    Поправил отступы

--- OrderManager/Program.cs
+++ OrderManager/Program.cs
@@ -4,60 +4,60 @@ try
 {
     Run();
 }
-catch (Exception ex)
+catch ( Exception ex )
 {
-    Console.WriteLine($"Ошибка: {ex.Message}");
+    Console.WriteLine( $"Ошибка: {ex.Message}" );
 }
 
 string AskProduct()
 {
-    Console.Write("Введите название товара: ");
+    Console.Write( "Введите название товара: " );
     return Console.ReadLine();
 }
 
 int AskCount()
 {
-    Console.Write("Введите колличество товара: ");
-    while (true)
+    Console.Write( "Введите колличество товара: " );
+    while ( true )
     {
-        if (int.TryParse(Console.ReadLine(), out int result) && result > 0)
+        if ( int.TryParse( Console.ReadLine(), out int result ) && result > 0 )
         {
             return result;
         }
         else
         {
-            Console.WriteLine("Это не число");
+            Console.WriteLine( "Это не число." );
         }
     }
 }
 
 string AskName()
 {
-    Console.Write("Введите имя получателя: ");
+    Console.Write( "Введите имя получателя: " );
     return Console.ReadLine();
 }
 
 string AskAdress()
 {
-    Console.Write("Введите адресс доставки: ");
+    Console.Write( "Введите адресс доставки: " );
     return Console.ReadLine();
 }
 
-bool ConfirmOrder(string name, int count, string product, string address)
+bool ConfirmOrder( string name, int count, string product, string address )
 {
-    Console.WriteLine($"Здравствуйте, {name}, вы заказали {count} {product} на адрес {address}, все верно? (yes/no)");
+    Console.WriteLine( $"Здравствуйте, {name}, вы заказали {count} {product} на адрес {address}, все верно? (yes/no)" );
     string response = Console.ReadLine();
     return response == "yes" ? true : false;
 }
 
-void ShowOrder(string name, string product, int count, string address, DateTime data)
+void ShowOrder( string name, string product, int count, string address, DateTime data )
 {
-    Console.WriteLine($"{name}! Ваш заказ {product} в количестве {count} оформлен! Ожидайте доставку по адресу {address} к {data}");
+    Console.WriteLine( $"{name}! Ваш заказ {product} в количестве {count} оформлен! Ожидайте доставку по адресу {address} к {data}" );
 }
 
-bool IsValidOrder(string name, string product, string address)
+bool IsValidOrder( string name, string product, string address )
 {
-    if (name == string.Empty || product == string.Empty || address == string.Empty)
+    if ( name == string.Empty || product == string.Empty || address == string.Empty )
     {
         return false;
     }
@@ -65,9 +65,9 @@ bool IsValidOrder(string name, string product, string address)
 }
 static void ShowOptions()
 {
-    Console.WriteLine("1. Заказать товар");
-    Console.WriteLine("2. Закрыть приложение");
-    Console.Write("Выберите опцию: ");
+    Console.WriteLine( "1. Заказать товар" );
+    Console.WriteLine( "2. Закрыть приложение" );
+    Console.Write( "Выберите опцию: " );
 }
 
 void ProcessOrder()
@@ -76,50 +76,50 @@ void ProcessOrder()
     int count = AskCount();
     string name = AskName();
     string address = AskAdress();
-    DateTime dateTime = DateTime.Today.AddDays(3);
-    if (!IsValidOrder(name, product, address))
+    DateTime dateTime = DateTime.Today.AddDays( 3 );
+    if ( !IsValidOrder( name, product, address ) )
     {
-        Console.WriteLine("Ваш заказ некорректен.");
+        Console.WriteLine( "Ваш заказ некорректен." );
         return;
     }
-    if (ConfirmOrder(name, count, product, address))
+    if ( ConfirmOrder( name, count, product, address ) )
     {
-        ShowOrder(name, product, count, address, dateTime);
+        ShowOrder( name, product, count, address, dateTime );
     }
     else
     {
-        Console.WriteLine("Заказ отменён.");
+        Console.WriteLine( "Заказ отменён." );
     }
 }
 
-void HandelrOperation(Operation? operation)
+void HandelrOperation( Operation? operation )
 {
-    switch (operation)
+    switch ( operation )
     {
         case Operation.Order:
             ProcessOrder();
             break;
         case Operation.Exit:
-            Console.WriteLine("Программа завершена.");
+            Console.WriteLine( "Программа завершена." );
             break;
         default:
-            throw new Exception("Неизвестная команда.");
+            throw new Exception( "Неизвестная команда." );
     }
 }
 Operation? GetOperation()
 {
     string operationStr = Console.ReadLine();
-    bool isParse = Enum.TryParse(operationStr, out Operation operation);
+    bool isParse = Enum.TryParse( operationStr, out Operation operation );
     return isParse ? operation : null;
 }
 void Run()
 {
     Operation? operation = null;
-    while ( operation != Operation.Exit)
+    while ( operation != Operation.Exit )
     {
         ShowOptions();
         operation = GetOperation();
-        HandelrOperation(operation);
+        HandelrOperation( operation );
     }
 }
 
